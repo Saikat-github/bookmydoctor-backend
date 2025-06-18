17526f76d5bf57f3d3a526225b9626aac6d9c3e6
Saikat <saikatsaha.rph@gmail.com>
2025-06-18 12:16:09 +0530

    added a route to ping the backend every 5 minutes

--- server.js
+++ server.js
@@ -118,6 +118,9 @@ app.use((err, req, res, next) => {
 app.use('/api/admin', adminRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/user", userRouter);
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
 
 
 
